be3e827fc5d2ccfb3b10a976a2502f26de7262b4
garciadaudt <garciadaudt@9b40d3aa-7619-0a97-90b2-adb40cc47f47>
2010-11-05 12:54:16 +0000

    Simple structure of the game

---
+++ trunk/src/Game/Chicken.java
@@ -0,0 +1,8 @@
+package Game;
+
+public class Chicken extends Actor {
+
+	public Chicken(double x, double y) {
+		super(x,y);
+	}
+}
---
+++ trunk/src/Game/Player.java
@@ -0,0 +1,8 @@
+package Game;
+
+public class Player extends Actor {
+
+	public Player(double x, double y) {
+		super(x,y);
+	}
+}
